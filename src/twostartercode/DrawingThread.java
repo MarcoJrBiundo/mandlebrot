734eaa9a6a181a6e274c15749ac299f251c78825
Marco Biundo <marco.biundo@mohawkcollege.ca>
2020-10-29 22:28:24 -0400

    comments added

--- src/twostartercode/DrawingThread.java
+++ src/twostartercode/DrawingThread.java
@@ -5,21 +5,31 @@ import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 
+/**
+ * Class that extends thread that will draw mandlebrot in sections(one section per thread)
+ */
 public class DrawingThread extends Thread {
     private static double MANDELBROT_RE_MIN = -2;
     private static double MANDELBROT_RE_MAX = 1;
     private static double MANDELBROT_IM_MIN = -1.2;
     private static double MANDELBROT_IM_MAX = 1.2;
+    //If the current thread is drawing a starting point other than 0 on x axis
     private static int splitPostion = 0;
 
 
-
+    /**
+     * Empty constructor
+     */
     public DrawingThread() {}
 
+    /**
+     * Run method which will go on its own thread
+     */
     public void run() {
-
         double precision = Math.max((MANDELBROT_RE_MAX - MANDELBROT_RE_MIN) / 700, (MANDELBROT_IM_MAX - MANDELBROT_IM_MIN) / 600);
         int convergenceSteps = 50;
+
+        //If current column is starting at x axis 0 run normally until split position (aka column is ending)
         if(splitPostion == 0){
         for (double c = MANDELBROT_RE_MIN, xR = TwoStarterCode.splits.get(splitPostion); xR < TwoStarterCode.splits.get(splitPostion + 1); c = c + precision, xR++) {
             for (double ci = MANDELBROT_IM_MIN, yR = 0; yR < 600; ci = ci + precision, yR++) {
@@ -33,17 +43,17 @@ public class DrawingThread extends Thread {
                 } else {
                     TwoStarterCode.canvas.getGraphicsContext2D().setFill(Color.PURPLE); // Convergence Color
                 }
-
-
                 TwoStarterCode.canvas.getGraphicsContext2D().fillRect(xR, yR, 1, 1);
-
             }
         }
+        //if split position is greater than  0 this means variable c will need to be adjusted for starting at a certian point in the image
         }else{
+            //determine what c should actually be at this point in the algorithm
             double m = -2.0;
             for (int i = 0; i < TwoStarterCode.splits.get(splitPostion); i++) {
                 m = m + 0.004285714285714286;
             }
+            //Run algorithm with new c value and draw from split position to end of split position
             for (double c = m, xR = TwoStarterCode.splits.get(splitPostion); xR < TwoStarterCode.splits.get(splitPostion + 1); c = c + precision, xR++) {
                 for (double ci = MANDELBROT_IM_MIN, yR = 0; yR < 600; ci = ci + precision, yR++) {
                     double convergenceValue = checkConvergence(ci, c, convergenceSteps);
@@ -60,11 +70,17 @@ public class DrawingThread extends Thread {
                 }
             }
         }
+        //Not thread safe but threads are join and has shown no problems in testing
         splitPostion ++;
     }
 
-
-
+    /**
+     * Determines shade of pixel
+     * @param ci
+     * @param c
+     * @param convergenceSteps
+     * @return
+     */
     private int checkConvergence(double ci, double c, int convergenceSteps) {
         double z = 0;
         double zi = 0;
--- src/twostartercode/TwoStarterCode.java
+++ src/twostartercode/TwoStarterCode.java
@@ -31,33 +31,28 @@ public class TwoStarterCode extends Application {
     private static double MANDELBROT_RE_MAX = 1;       
     private static double MANDELBROT_IM_MIN = -1.2;    
     private static double MANDELBROT_IM_MAX = 1.2;
-    //Canvas
+    //Canvas on which to draw
     public static Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+    //!!!!Choose number of threads here!!!!!!
     public static final int numberOfWorkers = 10;
+    // This variable holds the vertical split points for which the threads will draw there specified section
     public static ArrayList<Integer> splits = new ArrayList<>();
 
- 
+    /**
+     * Method that starts and shows the mandlebrot
+     * this method calls threads which draws the mandlebrot in columns
+     * @param primaryStage
+     * @throws InterruptedException
+     */
     @Override
     public void start(Stage primaryStage) throws InterruptedException {
-
         Pane fractalRootPane = new Pane();
-
+        //Create the splits array so threads can draw in their specified Columns
         int numberOfSplits = numberOfWorkers;
         int splitValues = 700 /numberOfSplits;
-        for(int i = 0; i <= 700; i= i + splitValues) {
-            splits.add(i);
-        }
+        for(int i = 0; i <= 700; i= i + splitValues) { splits.add(i); }
 
-
-
-
-
-
-
-
-
-
-
+        //Create Threads run them and join them so they run in specific order
         for(int i = 0; i < numberOfWorkers; i ++) {
             DrawingThread dt = new DrawingThread();
             dt.run();
@@ -66,9 +61,8 @@ public class TwoStarterCode extends Application {
 
 
 
-
+        //Drawing on the canvas I assume
         fractalRootPane.getChildren().add(canvas);
- 
         Scene scene = new Scene(fractalRootPane, CANVAS_WIDTH + 2 * X_OFFSET, CANVAS_HEIGHT + 2 * Y_OFFSET);
         scene.setFill(Color.BLACK);
         primaryStage.setTitle("Mandelbrot Set");
@@ -76,7 +70,10 @@ public class TwoStarterCode extends Application {
         primaryStage.show();
     }
 
- 
+    /**
+     * Main run method
+     * @param args
+     */
     public static void main(String[] args) {
         launch(args);
     }
